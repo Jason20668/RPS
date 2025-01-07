9c447ab2f3b22eb4b6fdfe641cf71a07a68ba79f
Jason20668 <526JBach@frhsd.com>
2025-01-07 10:13:12 -0500

    Btn images added (not done)

--- script.js
+++ script.js
@@ -3,7 +3,5 @@ let userName = prompt('Whats your name?')
 let Greet = document.getElementById('greeting')
 
 
-//Put username in website
-userName 
-? (Greet.innerText=`Hello, ${userName}`)
-: (Greet.innerText=`Your name is Jeremiah now hello Jeremiah`)
+
+
