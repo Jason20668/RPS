f82be3978d8a4fa2581935d14641be92e808038c
Jason20668 <526JBach@frhsd.com>
2025-01-07 10:42:49 -0500

    Computer choice almost done

--- script.js
+++ script.js
@@ -3,5 +3,34 @@ const userName = prompt('Whats your name?')
 
 const uName = `${userName}`
 
+function Computer = {
+    let randomNumber = Math.floor(Math.random() * 7)
+    //All possible answers
+    switch (randomNumber) {
+        case 0:
+            return 'Rock'
+            break;
+        case 1:
+            return 'Paper'
+            break;
+        case 2:
+            return 'Scissor'
+            break;
+        case 3:
+            return 'Gun'
+            break;
+        case 4:
+            return 'Rock'
+            break;
+        case 5:
+            return 'Paper'
+            break;
+        case 6:
+            return 'Scissor'
+            break;
+        }
+}
+
+const computerChoice = 
 
 
