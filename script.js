adc8a094c2cf2fa3231157c7aaec165d8c1fb7d3
Jason20668 <526JBach@frhsd.com>
2025-01-31 09:17:14 -0500

    nvm it broke things i dont wanna fix

--- script.js
+++ script.js
@@ -36,34 +36,20 @@ function updateScoreImages() {
         imageOne.src = 'IMGS/GCircle.png';
     } else if (userScore === 2) {
         imageTwo.src = 'IMGS/GCircle.png';
-    } else if (userScore === 3 && computerScore === 0) {
-        imageThree.src = 'IMGS/GCircle.png';
-        setTimeout(() => alert("You absolutely destroyed the computer!!!"), 750);
-        setTimeout(() => location.reload(), 1600); 
-    } else if (userScore === 3 && computerScore === 1) {
+    } else if (userScore === 3) {
         imageThree.src = 'IMGS/GCircle.png';
         setTimeout(() => alert("You beat the computer!!!"), 750);
         setTimeout(() => location.reload(), 1600); 
-    } else if (userScore === 3 && computerScore === 2) {
-        imageThree.src = 'IMGS/GCircle.png';
-        setTimeout(() => alert("You almost lost you kinda suck. But congrats on still winning!!!"), 750);
-        setTimeout(() => location.reload(), 1600); 
+    }
+
     // Update computer score
     if (computerScore === 1) {
         imageFour.src = 'IMGS/GCircle.png';
     } else if (computerScore === 2) {
         imageFive.src = 'IMGS/GCircle.png';
-    } else if (computerScore === 3 && userScore === 0) {
+    } else if (computerScore === 3) {
         imageSix.src = 'IMGS/GCircle.png';
-        setTimeout(() => alert("You got destroyed by a computer...loser"), 750);
-        setTimeout(() => location.reload(), 1600); 
-    } else if (computerScore === 3 && userScore === 1) {
-        imageSix.src = 'IMGS/GCircle.png';
-        setTimeout(() => alert("You only won one time damn you suck"), 750);
-        setTimeout(() => location.reload(), 1600); 
-    } else if (computerScore === 3 && userScore === 2) {
-        imageSix.src = 'IMGS/GCircle.png';
-        setTimeout(() => alert("At least you almost won...but you still suck"), 750);
+        setTimeout(() => alert("You lost to a computer...loser"), 750);
         setTimeout(() => location.reload(), 1600); 
     }
 }
